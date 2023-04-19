b3372e3c13bfee338ca8a6b3d9979624a802703d
Bubalehich <janeridebmx@gmail.com>
2023-04-19 13:55:31 +0300

    feat:add entity

---
+++ src/main/java/ru/clevertec/entity/Tag.java
@@ -0,0 +1,15 @@
+package ru.clevertec.entity;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+public class Tag {
+
+    private int id;
+
+    private String name;
+}
