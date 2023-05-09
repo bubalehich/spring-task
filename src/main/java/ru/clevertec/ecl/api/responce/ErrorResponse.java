c2d8caf3403ac064c2c06cdce0408f516604bd5a
Bubalehich <janeridebmx@gmail.com>
2023-05-09 19:50:08 +0300

    fix: remove annotations from record classes

--- src/main/java/ru/clevertec/ecl/api/responce/ErrorResponse.java
+++ src/main/java/ru/clevertec/ecl/api/responce/ErrorResponse.java
@@ -1,13 +1,4 @@
 package ru.clevertec.ecl.api.responce;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-@NoArgsConstructor
-@AllArgsConstructor
-@Getter
-@Setter
 public record ErrorResponse(int code, String message) {
 }
